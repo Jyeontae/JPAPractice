800c2056ef560880da68fc7dfb1fc7e9158a8148
Yeontae Jeong <jyeontae>
2022-08-25 14:33:59 +0900

    jpa 실습: 연관관계 매핑 전

---
+++ src/main/java/japbook/jpashop/domain/Order.java
@@ -0,0 +1,48 @@
+package japbook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private  Long id;
+    private  Long memberId;
+    private  Date orderDate;
+    private  OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
